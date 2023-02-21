ea3ca7422d663206eb394e63e88e682c836b8ba3
Denis <deniskkaa9@gmail.com>
2023-02-21 17:21:49 +0300

    Added formula and arguments with console enter

--- src/test.java
+++ src/test.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
         System.out.println("Hello, " + args[0]);
+
+        System.out.println("Введите пожалуйста два вещественных числа х и у: ");
+        double x = scan.nextDouble();
+        double y = scan.nextDouble();
+        double result = (3 * Math.pow(Math.cos(x - (Math.PI/6)),2)) / (0.5 + Math.sin(Math.pow(y,2)));
+        System.out.println("Ваш результат " + result);
     }
 }
