e7ea076135fee7a25d378fc197ec66b071ae2aaa
Denis <deniskkaa9@gmail.com>
2023-02-28 09:46:18 +0300

    Added MathClient and MathServer, all arguments are taking from console, bidlocode but made more readable :(

--- src/MathClient.java
+++ src/MathClient.java
@@ -20,8 +20,48 @@ public class MathClient {
             Scanner scanner = new Scanner(System.in);
             String v = scanner.nextLine();
             output.writeUTF(v); // send a message to the server
-            String receivedMsg = input.readUTF(); // read the reply from the server
 
+
+            switch (Integer.parseInt(v)){
+                case (1), (5) -> {
+                    System.out.println(input.readUTF());
+                    String a = scanner.nextLine();
+                    String n = scanner.nextLine();
+                    String x = scanner.nextLine();
+                    String b = scanner.nextLine();
+                    String c = scanner.nextLine();
+                    output.writeUTF(a);
+                    output.writeUTF(n);
+                    output.writeUTF(x);
+                    output.writeUTF(b);
+                    output.writeUTF(c);
+                }
+                case(2), (3) -> {
+                    System.out.println(input.readUTF());
+                    String x = scanner.nextLine();
+                    String y = scanner.nextLine();
+                    String a = scanner.nextLine();
+                    String w = scanner.nextLine();
+                    output.writeUTF(x);
+                    output.writeUTF(y);
+                    output.writeUTF(a);
+                    output.writeUTF(w);
+                }
+                case(4) -> {
+                    System.out.println(input.readUTF());
+                    String a = scanner.nextLine();
+                    String x = scanner.nextLine();
+                    output.writeUTF(a);
+                    output.writeUTF(x);
+                }
+                case(6), (7) -> {
+                    System.out.println(input.readUTF());
+                    String x = scanner.nextLine();
+                    output.writeUTF(x);
+                }
+            }
+
+            String receivedMsg = input.readUTF(); // read the reply from the server
             System.out.println("Received from the server: " + receivedMsg);
         } catch (IOException e) {
             e.printStackTrace();
--- src/MathServer.java
+++ src/MathServer.java
@@ -27,43 +27,62 @@ public class MathServer {
 
                     String v = input.readUTF();
                     double result = 0;
-                    double a = Double.parseDouble(args[2]);
-                    double a1 = Double.parseDouble(args[3]);
-                    double a2 = Double.parseDouble(args[4]);
-                    double b = Double.parseDouble(args[5]);
-                    double c = Double.parseDouble(args[6]);
-                    double x = Double.parseDouble(args[7]);
-                    double y = Double.parseDouble(args[8]);
-                    double w = Double.parseDouble(args[9]);
-                    double n = Double.parseDouble(args[10]);
-                    double d = Double.parseDouble(args[11]);
 
                     switch (Integer.parseInt(v)) {
                         case (1) -> {
+                            output.writeUTF( "Введите a, n, x, b, c : ");
+                            double a = Double.parseDouble(input.readUTF());
+                            double n = Double.parseDouble(input.readUTF());
+                            double x = Double.parseDouble(input.readUTF());
+                            double b = Double.parseDouble(input.readUTF());
+                            double c = Double.parseDouble(input.readUTF());
                             result = ((5 * Math.pow(a, n * x)) / (b + c)) - (Math.sqrt(Math.abs(Math.cos(Math.pow(x, 3)))));
                             System.out.println(result);
                         }
                         case (2) -> {
+                            output.writeUTF( "Введите x, y, a, w : ");
+                            double x = Double.parseDouble(input.readUTF());
+                            double y = Double.parseDouble(input.readUTF());
+                            double a = Double.parseDouble(input.readUTF());
+                            double w = Double.parseDouble(input.readUTF());
                             result = (Math.abs(x - y) / Math.pow((1 + 2 * x), a)) - Math.exp(Math.sqrt(1 + w));
                             System.out.println(result);
                         }
                         case (3) -> {
+                            output.writeUTF( "Введите a, a1, x, a2 : ");
+                            double a = Double.parseDouble(input.readUTF());
+                            double a1 = Double.parseDouble(input.readUTF());
+                            double x = Double.parseDouble(input.readUTF());
+                            double a2 = Double.parseDouble(input.readUTF());
                             result = Math.sqrt(a + a1 * x + a2 * Math.pow(Math.sqrt(Math.abs(Math.sin(x))), 0.33));
                             System.out.println(result);
                         }
                         case (4) -> {
+                            output.writeUTF( "Введите a, x : ");
+                            double a = Double.parseDouble(input.readUTF());
+                            double x = Double.parseDouble(input.readUTF());
                             result = Math.log(Math.abs(Math.pow(a, 7))) + Math.atan(x * x) + (Math.PI / (Math.sqrt(Math.abs(a + x))));
                             System.out.println(result);
                         }
                         case (5) -> {
+                            output.writeUTF( "Введите a, b, c, d, x : ");
+                            double a = Double.parseDouble(input.readUTF());
+                            double b = Double.parseDouble(input.readUTF());
+                            double c = Double.parseDouble(input.readUTF());
+                            double d = Double.parseDouble(input.readUTF());
+                            double x = Double.parseDouble(input.readUTF());
                             result = Math.pow(Math.sqrt(Math.pow(a + b, 2) / c + d) + Math.exp(Math.sqrt(x + 1)), 1.0 / 5.0);
                             System.out.println(result);
                         }
                         case (6) -> {
+                            output.writeUTF( "Введите x : ");
+                            double x = Double.parseDouble(input.readUTF());
                             result = Math.exp((2.0 * Math.sin(4.0 * x) + Math.pow(Math.cos(Math.pow(x, 2.0)), 2.0))) / (3.0 * x);
                             System.out.println(result);
                         }
                         case (7) -> {
+                            output.writeUTF( "Введите x : ");
+                            double x = Double.parseDouble(input.readUTF());
                             result = 0.25 * (((1.0 + Math.pow(x, 2.0)) / (1.0 - x)) + 0.5 * Math.tan(x));
                             System.out.println(result);
                         }
